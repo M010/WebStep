b61dfd4af065b7677bd5186aa76ae9ee12837d4f
Moh <mi243@ya.ru>
2016-11-29 23:26:08 +0300

    2.1.11.2

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 mv /home/box/WebStep /home/box/web
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunico.d/test-django
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
 
